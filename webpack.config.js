358e7eda4c237b2f2880b508e9924515b9e9e4c6
zhuyuexing <s_zhuyuexing@wps.cn>
2018-11-21 18:40:11 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const contentBasePath = path.resolve(__dirname, 'dist');
+
+module.exports = {
+    mode: "development",
+    devtool: "inline-source-map",
+    entry: "./src/index.ts",
+    output: {
+        path: contentBasePath,
+        filename: "bundle.js"
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+
+    devServer: {
+        contentBase: contentBasePath,
+        compress: true,
+        hot: true,
+        port: 9000
+    }
+};
